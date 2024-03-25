803469e5860fa6464476e2c39044ce502c0c33a4
yellaiah derangala <yellaiahderangala@gmail.com>
2024-03-25 21:00:11 +0530

    uploading first

--- mongodb.sh
+++ mongodb.sh
@@ -31,5 +31,17 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo &>> LOGFILE
 
 VALIDATE $? " copied mongodb repo"
 
-dnf install mongodb-org -y 
+dnf install mongodb-org -y &>> $LOGFILE
 VALIDATE $? " installing mongodb"
+
+systemctl enable mongod &>> $LOGFILE
+VALIDATE $? " enabling mongodb"
+
+systemctl start mongod &>> $LOGFILE 
+VALIDATE $? " starting  mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+VALIDATE $? " validate remote access to   mongodb"
+
+systemctl restart mongod &>> $LOGFILE
+VALIDATE $? " restarting   mongodb"
